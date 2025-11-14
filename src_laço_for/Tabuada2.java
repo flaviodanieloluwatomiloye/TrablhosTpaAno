685ffafe7fe4ebbd50a14eba590764144afbcf48
flaviodanieloluwatomiloye <flaviodanieloluwatomiloye@gmail.com>
2025-11-14 20:15:06 -0300

    Add files via upload

---
+++ src_laço_for/Tabuada2.java
@@ -0,0 +1,14 @@
+public class Tabuada2 {
+ 
+	public static void main(String[] args) {
+		int i, j, k;
+ 
+		
+		 for ( i = 1; i <= 10; i++) {
+            for (j = 1; j <= 10; j++) {
+            	 System.out.println( i+ " x " + j + " = " + (j * i));
+            }
+         }
+	}
+ 
+}
